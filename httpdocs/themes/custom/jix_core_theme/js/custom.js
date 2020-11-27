5bf086b98c79baad4fc3711a99971c7b6ac61d98
Medard Rebero <medard.rebero@gmail.com>
2020-11-27 17:35:57 +0100

    refactor

--- httpdocs/themes/custom/jix_core_theme/js/custom.js
+++ httpdocs/themes/custom/jix_core_theme/js/custom.js
@@ -3,15 +3,16 @@
  * Global utilities.
  *
  */
-(function($, Drupal) {
+(function ($, Drupal) {
 
   'use strict';
 
   Drupal.behaviors.jix_core_theme = {
-    attach: function(context, settings) {
-
-      // Custom code here
-
+    attach: function (context, settings) {
+      const isMobile = Modernizr.mq('(max-width: 767.98px)');
+      if (isMobile) {
+        $(context).find('form#views-exposed-form-jobs-display-page-search-result-page > div.row > div').addClass('mb-2');
+      }
     }
   };
 
