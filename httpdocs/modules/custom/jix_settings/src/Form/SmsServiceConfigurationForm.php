6728a742602dbc8655e4c2ef988f2d80d12fd946
Medard Rebero <medard.rebero@gmail.com>
2021-01-15 13:10:27 +0100

    Refactor

--- httpdocs/modules/custom/jix_settings/src/Form/SmsServiceConfigurationForm.php
+++ httpdocs/modules/custom/jix_settings/src/Form/SmsServiceConfigurationForm.php
@@ -40,30 +40,45 @@ class SmsServiceConfigurationForm extends ConfigFormBase
       '#collapsible' => false,
       '#collapsed' => false,
     );
-    $form['ftp_settings']['mtarget_ftp_host'] = array(
+    $form['ftp_settings']['ftp_protocol'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Protocol'),
+      '#options' => ['ftp' => 'FTP', 'sftp' => 'SFTP'],
+      '#default_value' => $config->get('ftp_protocol'),
+      '#required' => true,
+      '#description' => $this->t('File transfer protocol')
+    ];
+    $form['ftp_settings']['ftp_host'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Host'),
-      '#default_value' => $config->get('mtarget_ftp_host'),
+      '#default_value' => $config->get('ftp_host'),
       '#required' => true,
       '#description' => $this->t('Host server name')
     );
-    $form['ftp_settings']['mtarget_ftp_port'] = array(
+    $form['ftp_settings']['ftp_port'] = array(
       '#type' => 'number',
       '#title' => $this->t('Port'),
-      '#default_value' => $config->get('mtarget_ftp_port'),
+      '#default_value' => $config->get('ftp_port'),
       '#required' => true,
       '#description' => $this->t('Host server port')
     );
-    $form['ftp_settings']['mtarget_ftp_username'] = array(
+    $form['ftp_settings']['ftp_directory'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Directory'),
+      '#default_value' => empty($config->get('ftp_directory')) ? '/' : $config->get('ftp_directory'),
+      '#required' => true,
+      '#description' => $this->t('Directory path')
+    );
+    $form['ftp_settings']['ftp_username'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Username'),
-      '#default_value' => $config->get('mtarget_ftp_username'),
-      '#description' => $this->t('SFTP server username. Leave blank if not required')
+      '#default_value' => $config->get('ftp_username'),
+      '#description' => $this->t('Server username. Leave blank if not required')
     );
-    $form['ftp_settings']['mtarget_ftp_password'] = array(
+    $form['ftp_settings']['ftp_password'] = array(
       '#type' => 'password',
       '#title' => $this->t('Password'),
-      '#default_value' => $config->get('mtarget_ftp_password'),
+      '#default_value' => $config->get('ftp_password'),
       '#description' => $this->t('The currently set password is hidden for security reasons.')
     );
     return parent::buildForm($form, $form_state);
@@ -82,32 +97,56 @@ class SmsServiceConfigurationForm extends ConfigFormBase
 
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
-    $host = $form_state->getValue('mtarget_ftp_host');
-    $port = intval($form_state->getValue('mtarget_ftp_port'));
-    $username = $form_state->getValue('mtarget_ftp_username');
-    $password = $form_state->getValue('mtarget_ftp_password');
+    $protocol = $form_state->getValue('ftp_protocol');
+    $host = $form_state->getValue('ftp_host');
+    $port = intval($form_state->getValue('ftp_port'));
+    $directory = $form_state->getValue('ftp_directory');
+    $username = $form_state->getValue('ftp_username');
+    $password = $form_state->getValue('ftp_password');
     if (empty($password)) {
-      $password = Drupal::config(static::SETTINGS)->get('mtarget_ftp_password');
+      $password = Drupal::config(static::SETTINGS)->get('ftp_password');
     }
 
     $this->configFactory->getEditable(static::SETTINGS)
       ->set('number_daily_jobs', $form_state->getValue('number_daily_jobs'))
-      ->set('mtarget_ftp_host', $host)
-      ->set('mtarget_ftp_port', $port)
-      ->set('mtarget_ftp_username', $username)
-      ->set('mtarget_ftp_password', $password)
+      ->set('ftp_protocol', $protocol)
+      ->set('ftp_host', $host)
+      ->set('ftp_port', $port)
+      ->set('ftp_directory', $directory)
+      ->set('ftp_username', $username)
+      ->set('ftp_password', $password)
       ->save();
     parent::submitForm($form, $form_state);
 
-    $sftp = new SFTP($host, $port);
-    $loggedIn = $sftp->login($username, $password);
-    if (false === $loggedIn) {
-      Drupal::messenger()->addWarning('Connection to SFTP server could not be verified...');
-    } else {
-      Drupal::messenger()->addStatus('Connection to SFTP server is successful...');
-      if ($sftp->isConnected()) {
-        $sftp->disconnect();
-      }
+    switch ($protocol) {
+      case 'ftp':
+        $connection = ftp_connect($host, $port, 30);
+        if ($connection !== false) {
+          if (ftp_login($connection, $username, $password)) {
+            Drupal::messenger()->addStatus('Connection to FTP server is successful...');
+            if (ftp_chdir($connection, $directory) === false) {
+              Drupal::messenger()->addWarning('Directory folder not found.');
+            }
+          } else {
+            Drupal::messenger()->addWarning('Connection to FTP server could not be verified...');
+          }
+        }
+        break;
+      case 'sftp':
+        $sftp = new SFTP($host, $port);
+        $loggedIn = $sftp->login($username, $password);
+        if ($loggedIn) {
+          Drupal::messenger()->addStatus('Connection to SFTP server is successful...');
+          if ($sftp->chdir($directory) === false) {
+            Drupal::messenger()->addWarning('Directory folder not found.');
+          }
+          if ($sftp->isConnected()) {
+            $sftp->disconnect();
+          }
+        } else {
+          Drupal::messenger()->addWarning('Connection to SFTP server could not be verified...');
+        }
+        break;
     }
   }
 }
