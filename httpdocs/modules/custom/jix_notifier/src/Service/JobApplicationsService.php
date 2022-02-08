93f5d185489ffce610b6345ab59ecad42b242b22
Rebero <rmedard5@gmail.com>
2022-02-09 00:05:45 +0100

    Install JobinTz

--- httpdocs/modules/custom/jix_notifier/src/Service/JobApplicationsService.php
+++ httpdocs/modules/custom/jix_notifier/src/Service/JobApplicationsService.php
@@ -8,7 +8,9 @@ use Drupal\Core\Entity\EntityStorageException;
 use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
 use Drupal\Core\File\FileUrlGeneratorInterface;
 use Drupal\file\Entity\File;
+use Drupal\file\FileInterface;
 use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
 use Drupal\taxonomy\Entity\Term;
 use Drupal\taxonomy\TermInterface;
 use Drupal\webform\Entity\WebformSubmission;
@@ -29,29 +31,40 @@ class JobApplicationsService
 
   public function sendToCvSearch(WebformSubmission $jobApplication, string $cvSearchUrl)
   {
-    $data = $this->getCvSearchJsonData($jobApplication);
-    try {
-      $response = Drupal::httpClient()->post($cvSearchUrl, ['json' => $data]);
-      if ($response->getStatusCode() == 200) {
-        try {
-          $jobApplication->setElementData('field_application_sync', 'Yes');
-          $jobApplication->save();
-          Drupal::logger($this->channel)->info('Job application {'. $jobApplication->id() .'} sent to CV Search');
-        } catch (EntityStorageException $e) {
-          Drupal::logger($this->channel)->error('Saving application failed: ' . $e->getMessage());
-        }
-      } else {
-        Drupal::logger($this->channel)->error(t('Synchronizing application @id failed with error code @code: @message',
-          [
-            '@id' => $jobApplication->id(),
-            '@code' => $response->getStatusCode(),
-            '@message' => $response->getReasonPhrase()
-          ]));
+    $validationEntity = $jobApplication->validate();
+    if ($validationEntity->count() > 0) {
+      $messages = [];
+      foreach ($validationEntity->getEntityViolations() as $violation) {
+        $messages[] = $violation->getMessage();
       }
-    } catch (ClientException $exception) {
-      Drupal::logger($this->channel)->error('Cv Search Client Exception: ' . $exception->getMessage());
-    } catch (RequestException | ServerException $exception) {
-      Drupal::logger($this->channel)->error('Cv Search Request Exception: ' . $exception->getMessage() . ' | Request body: ' . Json::encode($data));
+      Drupal::logger($this->channel)->error('Invalid job submission: | Error Messages: ' . Json::encode($messages));
+    } else {
+      if ($validationEntity->count() == 0) {
+        $data = $this->getCvSearchJsonData($jobApplication);
+        try {
+          $response = Drupal::httpClient()->post($cvSearchUrl, ['json' => $data]);
+          if ($response->getStatusCode() == 200) {
+            try {
+              $jobApplication->setElementData('field_application_sync', 'Yes');
+              $jobApplication->save();
+              Drupal::logger($this->channel)->info('Job application {'. $jobApplication->id() .'} sent to CV Search');
+            } catch (EntityStorageException $e) {
+              Drupal::logger($this->channel)->error('Saving application failed: ' . $e->getMessage());
+            }
+          } else {
+            Drupal::logger($this->channel)->error(t('Synchronizing application @id failed with error code @code: @message',
+              [
+                '@id' => $jobApplication->id(),
+                '@code' => $response->getStatusCode(),
+                '@message' => $response->getReasonPhrase()
+              ]));
+          }
+        } catch (ClientException $exception) {
+          Drupal::logger($this->channel)->error('Cv Search Client Exception: ' . $exception->getMessage());
+        } catch (RequestException | ServerException $exception) {
+          Drupal::logger($this->channel)->error('Cv Search Request Exception: ' . $exception->getMessage() . ' | Request body: ' . Json::encode($data));
+        }
+      }
     }
   }
 
@@ -83,25 +96,23 @@ class JobApplicationsService
       }
     }
 
-    $cvFileId = intval($submission->getElementData('job_application_cv_resume_file'));
-    $experienceId = intval($submission->getElementData('job_application_experience'));
-    $diplomaId = intval($submission->getElementData('job_application_highest_degree'));
-    $studyId = intval($submission->getElementData('job_application_field_study'));
-
     $cvFileUrl = '';
-    if ($cvFileId > 0) {
+    $cvFileId = $submission->getElementData('job_application_cv_resume_file');
+    if (is_numeric($cvFileId)) {
       $fileUrlGenerator = Drupal::service('file_url_generator');
       if ($fileUrlGenerator instanceof FileUrlGeneratorInterface) {
         $cvFile = File::load($cvFileId);
-        if (!is_null($cvFile)) {
+        if ($cvFile instanceof FileInterface) {
           $cvFileUrl = $fileUrlGenerator->generateAbsoluteString($cvFile->getFileUri());
         }
       }
     }
 
+    $experienceId = $submission->getElementData('job_application_experience');
+    $diplomaId = $submission->getElementData('job_application_highest_degree');
+    $studyId = $submission->getElementData('job_application_field_study');
     $coverLetter = $submission->getElementData('job_application_cover');
     $coverLetter = empty($coverLetter) ? '' : strip_tags($coverLetter['value']);
-
     return [
       'DateReceived' => date('Y-m-d H:m:s', $submission->getCompletedTime()),
       'FirstName' => $submission->getElementData('job_application_firstname'),
@@ -109,14 +120,14 @@ class JobApplicationsService
       'Email' => $submission->getElementData('job_application_email'),
       'Tel' => $submission->getElementData('job_application_telephone'),
       'JobId' => $jobId,
-      'JobTitle' => is_null($job) ? '' : $job->getTitle(),
+      'JobTitle' => $job instanceof NodeInterface ? $job->getTitle() : '',
       'JobCategory' => substr_replace(trim($jobCategory), '', -1),
       'CoverNote' => $coverLetter,
       'Nationality' => $submission->getElementData('job_application_nationality'),
-      'Diploma' => $diplomaId > 0 ? Term::load($diplomaId)->getName() : '',
-      'Study' => $studyId > 0 ? Term::load($studyId)->getName() : '',
+      'Diploma' => is_numeric($diplomaId) ? Term::load($diplomaId)->getName() : '',
+      'Study' => is_numeric($studyId) ? Term::load($studyId)->getName() : '',
       'Languages' => substr_replace(trim($languages), '', -1),
-      'Experience' => $experienceId > 0 ? Term::load($experienceId)->getName() : '',
+      'Experience' => is_numeric($experienceId) ? Term::load($experienceId)->getName() : '',
       'Sex' => $submission->getElementData('job_application_sex'),
       'cvUrl' => $cvFileUrl
     ];
