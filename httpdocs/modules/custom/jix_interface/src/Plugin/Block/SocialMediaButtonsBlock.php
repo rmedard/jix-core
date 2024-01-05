8bb735873047c4b7677c8c675dcf6139a57084e5
medard <medard.rebero@gmail.com>
2024-01-05 16:45:49 +0100

    social medial field type change to url

--- httpdocs/modules/custom/jix_interface/src/Plugin/Block/SocialMediaButtonsBlock.php
+++ httpdocs/modules/custom/jix_interface/src/Plugin/Block/SocialMediaButtonsBlock.php
@@ -4,10 +4,8 @@
 namespace Drupal\jix_interface\Plugin\Block;
 
 
-use Drupal;
 use Drupal\Core\Block\Annotation\Block;
 use Drupal\Core\Block\BlockBase;
-use Drupal\jix_settings\Form\SocialMediaSettingsForm;
 use JetBrains\PhpStorm\ArrayShape;
 
 /**
@@ -22,29 +20,10 @@ use JetBrains\PhpStorm\ArrayShape;
 class SocialMediaButtonsBlock extends BlockBase
 {
 
-  #[ArrayShape([
-    '#theme' => "string",
-    '#facebook' => "string",
-    '#twitter' => "string",
-    '#instagram' => "string",
-    '#youtube' => "string",
-    '#linkedin' => "string",
-    '#whatsapp' => "string"])] public function build(): array
+  #[ArrayShape(['#theme' => "string"])] public function build(): array
   {
-    $facebook = Drupal::config(SocialMediaSettingsForm::SETTINGS)->get('facebook_page');
-    $instagram = Drupal::config(SocialMediaSettingsForm::SETTINGS)->get('instagram_page');
-    $twitter = Drupal::config(SocialMediaSettingsForm::SETTINGS)->get('twitter_page');
-    $youtube = Drupal::config(SocialMediaSettingsForm::SETTINGS)->get('youtube_page');
-    $linkedin = Drupal::config(SocialMediaSettingsForm::SETTINGS)->get('linkedin_page');
-    $whatsapp = Drupal::config(SocialMediaSettingsForm::SETTINGS)->get('whatsapp_call_link');
     return [
-      '#theme' => 'jix_social_media_buttons',
-      '#facebook' => trim($facebook),
-      '#twitter' => trim($twitter),
-      '#instagram' => trim($instagram),
-      '#youtube' => trim($youtube),
-      '#linkedin' => trim($linkedin),
-      '#whatsapp' => trim($whatsapp)
+      '#theme' => 'jix_social_media_buttons'
     ];
   }
 }
