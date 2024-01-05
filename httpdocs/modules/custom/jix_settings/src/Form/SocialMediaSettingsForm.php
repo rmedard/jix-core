33f35f7a8aafe960bb351e48a60d1c3de17ab1dd
medard <medard.rebero@gmail.com>
2024-01-05 15:23:58 +0100

    social medial field type change to url

--- httpdocs/modules/custom/jix_settings/src/Form/SocialMediaSettingsForm.php
+++ httpdocs/modules/custom/jix_settings/src/Form/SocialMediaSettingsForm.php
@@ -32,25 +32,25 @@ class SocialMediaSettingsForm extends ConfigFormBase
       '#description' => $this->t('The full URL to the target facebook page')
     ];
     $form['twitter_page'] = [
-      '#type' => 'textfield',
+      '#type' => 'url',
       '#title' => $this->t('Twitter page'),
       '#default_value' => $config->get('twitter_page'),
       '#description' => $this->t('The full URL to the target twitter page')
     ];
     $form['instagram_page'] = [
-      '#type' => 'textfield',
+      '#type' => 'url',
       '#title' => $this->t('Instagram page'),
       '#default_value' => $config->get('instagram_page'),
       '#description' => $this->t('The full URL to the target instagram page')
     ];
     $form['youtube_page'] = [
-      '#type' => 'textfield',
+      '#type' => 'url',
       '#title' => $this->t('Youtube page'),
       '#default_value' => $config->get('youtube_page'),
       '#description' => $this->t('The full URL to the target youtube page')
     ];
     $form['linkedin_page'] = [
-      '#type' => 'textfield',
+      '#type' => 'url',
       '#title' => $this->t('Linkedin page'),
       '#default_value' => $config->get('linkedin_page'),
       '#description' => $this->t('The full URL to the target linkedin page')
@@ -62,41 +62,6 @@ class SocialMediaSettingsForm extends ConfigFormBase
       '#description' => $this->t('The full link for whatsapp call')
     ];
     return parent::buildForm($form, $form_state);
-  }
-
-  public function validateForm(array &$form, FormStateInterface $form_state): void
-  {
-    if (!$form_state->isValueEmpty('facebook_page')) {
-      $isValid = filter_var($form_state->getValue('facebook_page'), FILTER_VALIDATE_URL);
-      if ($isValid === false) {
-        $form_state->setErrorByName('facebook_page', t('This must be a valid URL starting with http or https'));
-      }
-    }
-    if (!$form_state->isValueEmpty('twitter_page')) {
-      $isValid = filter_var($form_state->getValue('twitter_page'), FILTER_VALIDATE_URL);
-      if ($isValid === false) {
-        $form_state->setErrorByName('twitter_page', t('This must be a valid URL starting with http or https'));
-      }
-    }
-    if (!$form_state->isValueEmpty('instagram_page')) {
-      $isValid = filter_var($form_state->getValue('instagram_page'), FILTER_VALIDATE_URL);
-      if ($isValid === false) {
-        $form_state->setErrorByName('instagram_page', t('This must be a valid URL starting with http or https'));
-      }
-    }
-    if (!$form_state->isValueEmpty('youtube_page')) {
-      $isValid = filter_var($form_state->getValue('youtube_page'), FILTER_VALIDATE_URL);
-      if ($isValid === false) {
-        $form_state->setErrorByName('youtube_page', t('This must be a valid URL starting with http or https'));
-      }
-    }
-    if (!$form_state->isValueEmpty('linkedin_page')) {
-      $isValid = filter_var($form_state->getValue('linkedin_page'), FILTER_VALIDATE_URL);
-      if ($isValid === false) {
-        $form_state->setErrorByName('linkedin_page', t('This must be a valid URL starting with http or https'));
-      }
-    }
-    parent::validateForm($form, $form_state);
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state): void
